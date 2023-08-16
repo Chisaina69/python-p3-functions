5f5d64b67af005b3b03d6696b7dacc5136226919
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-08-16 18:19:10 +0300

    Completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+sum_result = add(1, 2)
+print(sum_result)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+
+result = halve(4)
+print(result)
+
+result = halve("two")
+print(result)
